678f26d762702b5749cdd2b933323efaa6b08195
Sérgio Vieira <sergiovieira.webb@gmail.com>
2022-03-11 15:03:17 -0300

    resolucao requisito 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,21 @@
 export async function getCategories() {
-  // Implemente aqui
+  try {
+    const endpoint = 'https://api.mercadolibre.com/sites/MLB/categories';
+    const request = await fetch(endpoint);
+    const data = await request.json();
+    return data;
+  } catch (error) {
+    return new Error(error);
+  }
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  try {
+    const endpoint = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+    const request = await fetch(endpoint);
+    const data = await request.json();
+    return data;
+  } catch (error) {
+    return new Error(error);
+  }
 }
