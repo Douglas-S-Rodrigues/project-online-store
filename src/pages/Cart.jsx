fbb409bfe6cb71128d707895ac35df533d646dc4
Lucas Mesquita <lucas.cas.mesquita@gmail.com>
2022-03-14 19:29:34 -0300

    Resolvendo requisito 8

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,24 +3,48 @@ import PropTypes from 'prop-types';
 import { getProductDetails } from '../services/api';
 
 class Cart extends React.Component {
-  render() {
+  constructor() {
+    super();
+
+    this.state = {
+      products: [],
+    };
+  }
+
+  componentDidMount() {
+    this.handleProducts();
+  }
+
+  handleProducts = () => {
     const { idItem } = this.props;
+    const arrayProducts = [];
+    idItem.map(async (item) => {
+      const getProducts = await getProductDetails(item);
+      arrayProducts.push(getProducts);
+    });
+    this.setState({
+      products: arrayProducts,
+    });
+  }
+
+  render() {
+    const { products } = this.state;
+    console.log(products);
     return (
       <div>
-        { idItem.length < 1
+        { products.length < 1
           ? <h3 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h3> : (
-            idItem.map(async (item) => (
-              <>
+            products.map((item) => (
+              <div key={ item.id }>
                 <p
                   data-testid="shopping-cart-product-name"
                 >
-                  { await getProductDetails(item).title }
+                  { item.title }
                 </p>
-                console.log( await getProductDetails(item))
-              </>
+              </div>
             ))
           )}
-        <p data-testid="shopping-cart-product-quantity">{ idItem.length }</p>
+        <p data-testid="shopping-cart-product-quantity">{ products.length }</p>
       </div>
     );
   }
