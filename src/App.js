f2eb36293de3eaa241d7dbf2eb70a9436f87efb8
Douglas Rodrigues <douglas.rodrigues.fw@gmail.com>
2022-03-14 18:41:32 -0300

    inicio do requisito 8

--- src/App.js
+++ src/App.js
@@ -5,16 +5,43 @@ import './App.css';
 import Cart from './pages/Cart';
 import Details from './pages/Details';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={ Home } />
-        <Route exact path="/details/:id" component={ Details } />
-        <Route exact path="/cart" component={ Cart } />
-      </Switch>
-    </BrowserRouter>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+      item: [],
+
+    };
+  }
+
+  handleAddItem = async (proId) => {
+    const { item } = this.state;
+    this.setState({
+      item: [...item, proId],
+    });
+  }
+
+  render() {
+    const { item } = this.state;
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={ () => <Home handleAddItem={ this.handleAddItem } /> }
+          />
+          <Route exact path="/details/:id" component={ Details } />
+          <Route
+            exact
+            path="/cart"
+            render={ () => <Cart idItem={ item } /> }
+          />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { getProductDetails } from '../services/api';
 
 class Cart extends React.Component {
   render() {
+    const { idItem } = this.props;
     return (
-      <h3 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h3>
+      <div>
+        { idItem.length < 1
+          ? <h3 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h3> : (
+            idItem.map(async (item) => (
+              <>
+                <p
+                  data-testid="shopping-cart-product-name"
+                >
+                  { await getProductDetails(item).title }
+                </p>
+                console.log( await getProductDetails(item))
+              </>
+            ))
+          )}
+        <p data-testid="shopping-cart-product-quantity">{ idItem.length }</p>
+      </div>
     );
   }
 }
+
+Cart.propTypes = {
+  idItem: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
 
 export default Cart;
